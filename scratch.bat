2cff1665bab6fe4e6064f0d6623e01e53d02499e
takureepers <141208490+takureepers@users.noreply.github.com>
2023-10-10 20:07:52 +0900

    Add files via upload

---
+++ scratch.bat
@@ -0,0 +1,52 @@
+@ECHO OFF
+title Scratch構築システム
+color 2
+
+start README.txt
+
+:INPUT_START
+ECHO +-------------------------------------------------------+
+ECHO  作成先のパスを入力してください。
+ECHO +-------------------------------------------------------+
+SET INPUT_STR=
+SET /P INPUT_STR=
+ 
+IF "%INPUT_STR%"=="" GOTO :INPUT_START
+ 
+:INPUT_CONF
+ECHO +-------------------------------------------------------+
+ECHO  入力した文字は[%INPUT_STR%]でよろしいですか？ (2GBほどの空き容量が必要となります。)
+ECHO （Y / N）
+ECHO +-------------------------------------------------------+
+SET CONF_SELECT=
+SET /P CONF_SELECT=
+ 
+IF "%CONF_SELECT%"== SET CONF_SELECT=Y
+IF /I NOT "%CONF_SELECT%"=="Y"  GOTO :INPUT_START
+ 
+:INPUT_END
+cd %INPUT_STR%
+ECHO +-------------------------------------------------------+
+ECHO  実行しています...
+ECHO +-------------------------------------------------------+
+git clone --depth 1 https://github.com/llk/scratch-vm.git
+git clone --depth 1 https://github.com/llk/scratch-gui.git
+ECHO +-------------------------------------------------------+
+ECHO  インストール完了。構築します....
+ECHO +-------------------------------------------------------+
+cd scratch-vm
+npm install
+npm link
+cd ../scratch-gui
+npm install
+npm link scratch-vm
+ECHO +-------------------------------------------------------+
+ECHO  最終段階です。ビルドしています...
+ECHO +-------------------------------------------------------+
+npm start
+ECHO +-------------------------------------------------------+
+ECHO  すべてが終了しました。localhost:8601を開きます。
+ECHO +-------------------------------------------------------+
+start https://localhost:8601
+pause
+stop
